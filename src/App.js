8c8e43ae438f601b19aced9d1af61fb27253e3c0
Rio254565 <r_kumar5@me.iitr.ac.in>
2024-09-09 22:28:15 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './homepage.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div className="navbar-content">
+          <a href="#home" className="nav-link">Home</a>
+          <a href="#category" className="nav-link">Category</a>
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </nav>
+      <div className="main-body">
+        <h1 className="overlay-text">Welcome to My Homepage</h1>
+      </div>
     </div>
   );
 }
